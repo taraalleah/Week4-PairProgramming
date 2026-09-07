b069cb0d767d3e088ff9ab0d45ef938fd394e3a9
Tara <mtaraalleah@gmail.com>
2026-09-07 15:35:29 +0300

    testing middleware auth.js

--- week4-be-pp/middleware/auth.js
+++ week4-be-pp/middleware/auth.js
@@ -1,6 +1,7 @@
-
 module.exports = (req, res, next) => {
     const isAdmin = req.query.admin === "true";
+    console.log(req.query.admin);
+
     if (!isAdmin) {
         return res.status(403).json({ message: "Admin access required" });
     }
